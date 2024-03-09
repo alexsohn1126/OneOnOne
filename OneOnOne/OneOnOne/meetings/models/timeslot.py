6763361e7a9d0738820b213f60f156c6221150ed
Junaid Arshad <jun.arshad@gmail.com>
2024-03-09 18:20:12 -0500

    Added attributes for timeslot model

---
+++ OneOnOne/OneOnOne/meetings/models/timeslot.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .calendar import Calendar
+
+class Timeslot(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE, related_name='timeslots')
+
+    def __str__(self):
+        return(f"{self.name} in {self.calendar} starting on {self.start_time} and ending on {self.start_time}")
