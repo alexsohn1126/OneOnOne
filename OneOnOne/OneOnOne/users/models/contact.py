4ef95a21d4e75e42e70120b1eb9352b22d63103c
Alex Sohn <alexsohn1126@gmail.com>
2024-03-09 20:25:44 -0500

    Create contact model

--- OneOnOne/OneOnOne/users/models/contact.py
+++ OneOnOne/OneOnOne/users/models/contact.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .user import User
 
 # Create your models here.
+class Contact:
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=120, unique = True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contact')
+    
+    def __str__(self):
+        return (f"Contact's name is {self.first_name} {self.last_name} and their email is {self.email}")
