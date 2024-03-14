586916c5c51b087449fe4d677e18a1f346f9bfc5
Emily Su <emilyzqsu@gmail.com>
2024-03-14 12:53:47 -0400

    Removed changes to User model

--- OneOnOne/OneOnOne/users/models/user.py
+++ OneOnOne/OneOnOne/users/models/user.py
@@ -5,8 +5,6 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     email = models.EmailField(max_length=120, unique = True)
-    user_permissions = None
-    groups = None
     
     def __str__(self):
         return (f"User's name is {self.first_name} {self.last_name} and their email is {self.email}")
