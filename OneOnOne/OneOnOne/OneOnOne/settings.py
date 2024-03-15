def5a4437a5528bb2ba01a489018ebdbb6284a21
Sadeed Ahmed <sadeedahmed24@gmail.com>
2024-03-14 20:33:18 -0400

    contacts_api

--- OneOnOne/OneOnOne/OneOnOne/settings.py
+++ OneOnOne/OneOnOne/OneOnOne/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
+    'rest_framework',
     'users',
-    'meetings'
+    'meetings', 
+    'contacts_api',
 ]
 
 MIDDLEWARE = [
