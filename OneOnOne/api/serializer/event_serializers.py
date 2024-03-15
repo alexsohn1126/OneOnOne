fabb7ae41adbec87dc2cc4cbb06fb6178c0de0a5
Alex Sohn <alexsohn1126@gmail.com>
2024-03-15 14:39:00 -0400

    Send email when finalizing event, and make event the only one confirmed in the timeslot

--- OneOnOne/api/serializer/event_serializers.py
+++ OneOnOne/api/serializer/event_serializers.py
@@ -1,3 +1,5 @@
+from django.core.mail import send_mail
+from django.utils.formats import date_format
 from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from meetings.models.timeslot import Timeslot
@@ -12,10 +14,28 @@ class EventSerializer(serializers.ModelSerializer):
     if self.instance and self.instance.timeslot != value:
       raise ValidationError("You may not edit timeslot")
     return value
+  
   def validate_contact(self, value):
     if self.instance and self.instance.contact != value:
       raise ValidationError("You may not edit contact")
     return value
+  
+  def update(self, instance, validated_data):
+    instance.timeslot.events.all().update(confirmed=False)
+    instance = super().update(instance, validated_data)
+    if instance.confirmed:
+      self.send_event_confirmed_email(instance.timeslot, instance.contact)
+    return instance
+  
+  def send_event_confirmed_email(self, timeslot, contact):
+    send_mail(
+      f'{contact.first_name}, Your Meeting with {timeslot.calendar.owner.first_name} is Finalized!',
+      f'Your meeting from {date_format(timeslot.start_time, "DATETIME_FORMAT")} to {date_format(timeslot.end_time, "DATETIME_FORMAT")} has been finalized.',
+      from_email='OneToOne@test.com',
+      recipient_list=[contact.email]
+    )
+
+
   class Meta:
     model = Event
     fields = ('id', 'timeslot', 'contact', 'confirmed')
