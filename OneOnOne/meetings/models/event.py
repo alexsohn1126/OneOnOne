4e2bf979e6ff9a2de56c6fab6b9e7a3724ae7abc
Junaid Arshad <jun.arshad@gmail.com>
2024-03-14 22:07:09 -0400

    Updated Event and TImeslot models to reflect attribute changes

--- OneOnOne/meetings/models/event.py
+++ OneOnOne/meetings/models/event.py
@@ -1,12 +1,12 @@
 from django.db import models
 from .timeslot import Timeslot
+from users.models.contact import Contact
 
 class Event(models.Model):
-    date = models.DateField()
-    start_time = models.DateTimeField()
-    duration = models.IntegerField(default=30)
     # timeslot to events relation: One time slot can be in many events so foreign key on the many side
     timeslot = models.ForeignKey(Timeslot, on_delete=models.CASCADE, related_name='events')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='contact')
+    confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return(f"Event in {self.timeslot} starting on {self.start_time}")
--- OneOnOne/meetings/models/timeslot.py
+++ OneOnOne/meetings/models/timeslot.py
@@ -2,12 +2,11 @@ from django.db import models
 from .calendar import Calendar
 
 class Timeslot(models.Model):
-    name = models.CharField(max_length=50)
-    date = models.DateField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     # foreign key on the many side
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE, related_name='timeslots')
+    high_priority = models.BooleanField(default=False)
 
     def __str__(self):
         return(f"{self.name} in {self.calendar} starting on {self.start_time} and ending on {self.start_time}")
