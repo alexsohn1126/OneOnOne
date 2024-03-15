d836d02b23fe11a3c1272b3165fb57452d25fe25
Emily Su <emilyzqsu@gmail.com>
2024-03-15 16:25:02 -0400

    updated admin.py in meetings

--- OneOnOne/meetings/admin.py
+++ OneOnOne/meetings/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models.calendar import Calendar
 from .models.event import Event
 from .models.timeslot import Timeslot
+from .models.invitee import Invitee
 
 # Register your models here.
 admin.site.register(Calendar)
 admin.site.register(Event)
 admin.site.register(Timeslot)
+admin.site.register(Invitee)
