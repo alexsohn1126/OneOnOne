5ce641e49bf37c34b129a53beaa39b0ab0637363
Alex Sohn <alexsohn1126@gmail.com>
2024-03-15 04:16:22 -0400

    Add event post api

--- OneOnOne/api/urls.py
+++ OneOnOne/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.event_views import EventListCreate 
+from .views.event_views import EventList, EventCreate
 from .views.post_views import PostList, PostDetail
 from .views.calendar_views import CalendarList, CalendarDetail, GetCalendarAddTimeslotView, TimeslotUpdateDestroy
 
@@ -8,7 +8,8 @@ app_name = "api"
 urlpatterns = [
     path('contacts/<int:pk>', PostDetail.as_view(), name="detailcreate"),
     path('contacts/', PostList.as_view(), name='listcreate'),
-    path('timeslots/<int:timeslot_id>/events', EventListCreate.as_view(), name='list_create_event'),
+    path('timeslots/<int:timeslot_id>/events/', EventList.as_view(), name='list_events'),
+    path('events/', EventCreate.as_view(), name='create_events'),
     path('calendars/<int:calendar_id>/', GetCalendarAddTimeslotView.as_view(), name="GetCalendarAddTimeslot"),
     path('calendars/', CalendarList.as_view(), name='calendars_list'),
     path('calendars/<int:calendar_id>/timeslots/<int:id>/', TimeslotUpdateDestroy.as_view(), name='timeslot_update_destroy'),
--- OneOnOne/api/views/event_views.py
+++ OneOnOne/api/views/event_views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from ..serializer.event_serializers import EventSerializer
 from meetings.models.event import Event
 from meetings.models.timeslot import Timeslot
+from users.models.contact import Contact
 
-class EventListCreate(generics.ListCreateAPIView):
+class EventList(generics.ListAPIView):
   queryset = Event.objects.all()
   serializer_class = EventSerializer
 
@@ -17,8 +18,29 @@ class EventListCreate(generics.ListCreateAPIView):
     if not request.user.is_authenticated:
       return Response('UNAUTHORIZED', status=status.HTTP_401_UNAUTHORIZED)
     timeslot = Timeslot.objects.filter(pk=timeslot_id)
+    # Make sure timeslot exists
     if not timeslot.exists():
       return Response('NOT FOUND', status=status.HTTP_404_NOT_FOUND)
+    # User must be the owner of the calendar to view events
     if request.user != timeslot.first().calendar.owner:
       return Response('FORBIDDEN', status=status.HTTP_403_FORBIDDEN)
     return super().get(request, timeslot_id=timeslot_id, *args, **kwargs)
+
+
+class EventCreate(generics.CreateAPIView):
+  queryset = Event.objects.all()
+  serializer_class = EventSerializer
+
+  def post(self, request, *args, **kwargs):
+    if not request.user.is_authenticated:
+      return Response('UNAUTHORIZED', status=status.HTTP_401_UNAUTHORIZED)
+    timeslot = Timeslot.objects.filter(pk=request.data.get('timeslot'))
+    contact = Contact.objects.filter(pk=request.data.get('contact'))
+    # Make sure timeslot and contact exists
+    if not timeslot.exists() or not contact.exists():
+      return Response('NOT FOUND', status=status.HTTP_404_NOT_FOUND)
+    # User must be the owner of the calendar AND the contact to create event
+    if (request.user != timeslot.first().calendar.owner or 
+        request.user != contact.first().user):
+      return Response('FORBIDDEN', status=status.HTTP_403_FORBIDDEN)
+    return super().post(request, *args, **kwargs)
