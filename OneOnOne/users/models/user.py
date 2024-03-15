24820d8f9c737707612ad9f269b3a9f8ad767485
Emily Su <emilyzqsu@gmail.com>
2024-03-15 01:53:36 -0400

    Updated User model

--- OneOnOne/users/models/user.py
+++ OneOnOne/users/models/user.py
@@ -5,6 +5,9 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     email = models.EmailField(max_length=120, unique = True)
+    username = models.CharField(max_length=150, unique=False, blank=True) # Make it so that username is not required 
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['first_name', 'last_name']
     
     def __str__(self):
         return (f"USER: {self.first_name} {self.last_name}, {self.email}")
