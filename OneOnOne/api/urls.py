c6137eb91fd7ad28ab45b6be6a9c9ae2b3f4e8a4
Junaid Arshad <jun.arshad@gmail.com>
2024-03-15 00:51:58 -0400

    Added a '/' to the end of all paths

--- OneOnOne/api/urls.py
+++ OneOnOne/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path 
 from .views.post_views import PostList, PostDetail
-from .views.calendar_views import CalendarList, CalendarDetail
+from .views.calendar_views import CalendarList, CalendarDetail, GetCalendarAddTimeslotView
 
 app_name = "api"
 
 urlpatterns = [
     path('contacts/<int:pk>', PostDetail.as_view(), name="detailcreate"),
     path('contacts/', PostList.as_view(), name='listcreate'),
-    path('calendars/<int:pk>', CalendarDetail.as_view(), name="detailcreate"),
-    path('calendars', CalendarList.as_view(), name='listcreate'),
+    path('calendars/<int:calendar_id>/', GetCalendarAddTimeslotView.as_view(), name="GetCalendarAddTimeslot"),
+    path('calendars/', CalendarList.as_view(), name='calendars_list'),
 ]
