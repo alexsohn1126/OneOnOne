29353bf02e45b2bc570c3a9c09d974180b66f679
Junaid Arshad <jun.arshad@gmail.com>
2024-04-08 09:18:39 -0400

    Styled create calendar button

--- oneononeapi/src/components/calendar.js
+++ oneononeapi/src/components/calendar.js
@@ -179,7 +179,17 @@ function ListCalendars({ data, setCalendars }) {
             <ul>{calendars}</ul>
 
             <div className="App">
-            <button onClick={toggleOverlay}>Create new calendar</button>
+            
+            <div class="py-2">
+                <button className="bg-green-3 hover:bg-green-2 text-white py-2 px-4 rounded-full inline-flex items-center" onClick={toggleOverlay}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 me-2">
+                        <path fillRule="evenodd"
+                        d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z"
+                        clipRule="evenodd" />
+                    </svg>
+                    Create Calendar
+                </button>
+            </div>
 
             <Overlay isOpen={isOpen} onClose={toggleOverlay}>
                 <form onSubmit={handleSubmit}>
